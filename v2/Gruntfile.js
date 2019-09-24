201ad83657b227d944546dc3c54c4dcdebf7d4db
Girish <girish.chennura@vaultbank.io>
2019-09-24 17:56:51 +0530

    Move existing website as version 2

---
+++ v2/Gruntfile.js
@@ -0,0 +1,58 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    postcss: {
+      options: {
+        map: false,
+        processors: [
+          require('precss')(), // allow you to use Sass-like CSS.
+          // require('pixrem')(), // add fallbacks for rem units.
+          // require('postcss-calc')(), // reduces calc() to values (when expressions involve the same units).
+          require('autoprefixer-core')({browsers: 'last 2 versions'}), // add vendor prefixes
+        ]
+      },
+      dist: {
+        files: {
+          'css/css.css': 'src/style.css'
+        }
+      }
+    },
+    cssmin: {
+      options: {
+        mergeIntoShorthands: false,
+        roundingPrecision: -1
+      },
+      target: {
+        files: {
+          // 'css/css.min.css': ['css/css.css', 'css/scss.css']
+          'css/css.min.css': 'css/css.css'
+        }
+      }
+    },
+    watch: {
+      css: {
+        files: ['src/*.css'],
+        tasks: ['postcss']
+      },
+    },
+    htmlmin: {                                     // Task
+      dist: {                                      // Target
+        options: {                                 // Target options
+          removeComments: true,
+          collapseWhitespace: true
+        },
+        files: {                                   // Dictionary of files
+          'index.html': 'index-source.html'        // 'destination': 'source'
+        }
+      }
+    }
+  });
+
+  // Load the plugins
+  grunt.loadNpmTasks('grunt-contrib-htmlmin');
+  grunt.loadNpmTasks('grunt-postcss');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  // Default tasks.
+  grunt.registerTask('default', ['htmlmin'], ['postcss'], ['cssmin']);
+};
---
+++ v2/README.md
@@ -0,0 +1,35 @@
+# my github site http://cgirish.github.io
+
+command to combine and export multiple post css(sass) files to css
+```shell
+grunt watch
+```
+
+command to minify css
+```shell
+grunt cssmin
+```
+
+
+command to minify html
+```shell
+grunt htmlmin
+```
+
+
+<!--
+# 1 YEAR
+Header set Cache-Control "max-age=29030400, public"
+
+# 1 MONTH
+Header set Cache-Control "max-age=2592000, public"
+
+# 1 WEEK
+Header set Cache-Control "max-age=604800, public"
+
+# 1 HOUR
+Header set Cache-Control "max-age=3600, public"
+
+# DON'T CACHE ANY FILE
+Header set Cache-Control "max-age=0, private, no-store, no-cache, must-revalidate"
+-->
